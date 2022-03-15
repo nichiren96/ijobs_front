fa727dedf76376664733cf03943d5c4e83bdc71e
Ianel Tombozafy <ianell.s118@gmail.com>
2022-03-15 14:38:18 +0300

    Finished login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,25 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { MainLayout } from "../../layouts/main_layout";
+import { AuthenticationService } from "../../services/authentication_service";
 
 export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const authService = new AuthenticationService();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    authService
+      .login({ email: email, password: password })
+      .then((result) => console.log(result))
+      .catch((error) => {
+        setIsError(true);
+        setErrorMsg(error.response.data.message);
+      });
+  };
+
   return (
     <MainLayout>
-      <div>
-        <h2>Connexion</h2>
-        <div>
-          <form>
-            <div>
-              <label>Email</label>
-              <input type="text" />
+      <div className="flex flex-col items-center">
+        <div className="p-8 shadow-2xl w-1/4">
+          <h2 className="text-center text-2xl">Connexion</h2>
+          <p className="text-red-500 mt-2">{isError && errorMsg}</p>
+          <div className="flex flex-col items-center w-full">
+            <form className="w-full" onSubmit={handleSubmit}>
+              <div className="flex flex-col items-start mt-4">
+                <label htmlFor="email">Email</label>
+                <input
+                  autoComplete="off"
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+                />
+              </div>
+              <div className="flex flex-col items-start mt-4">
+                <label htmlFor="password">Mot de passe</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className="w-full border border-gray-300 px-3 py-2 rounded focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+                />
+              </div>
+              <div className="mt-4 flex justify-center">
+                <button
+                  type="submit"
+                  className="w-full shadow-md py-3 px-6 rounded-md transition duration-300 bg-indigo-600 text-white"
+                >
+                  Se connecter
+                </button>
+              </div>
+            </form>
+            <div className="mt-4 flex justify-center">
+              <p className="">
+                <a href="#" className="hover:text-indigo-600">
+                  Créer un compte
+                </a>
+              </p>
             </div>
-            <div>
-              <label>Mot de passe</label>
-              <input type="password" />
-            </div>
-            <div>
-              <button type="submit">Se connecter</button>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     </MainLayout>
